57972ede1d78307a59e88a5d0ef4188ecd33fed2
Caliph Dev <92326690+caliphdev@users.noreply.github.com>
2023-02-26 06:27:17 +0000

    idk

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/files", express.static(path.join(__dirname, 'temporary')));
 app.set('views', path.join(__dirname, 'views'));
 
 // Multer ( File Upload )
@@ -41,12 +42,19 @@ app.get('/', (req, res) => {
 // Upload Image
 app.post("/api/process/nobg", upload.single('image'), async (req, res) => {
     if (req.file) {
-        if (req.file.mimetype != "image/png" && req.file.mimetype != "image/jpeg") return res.json({ error: "Only PNG and JPEG Image Allowed" });
+        if (req.file.mimetype != "image/png" && req.file.mimetype != "image/jpeg") {
+           fs.unlinkSync(req.file.path);
+          return res.json({ error: "Only PNG and JPEG Image Allowed" });
+        }
         try {
             processImages = await processImage(req.file.path);
-            res.json({ image: "data:image/png;base64," + processImages.toString('base64') });
+        fileid = crypto.randomBytes(16).toString('hex') + path.extname(req.file.originalname);
+            dts = "./temporary/"+fileid;
+            await fs.promises.writeFile(dts, processImages);
+            res.json({ image: "/files/"+ fileid });
         } catch (error) {
             res.json({ error: "Unknown Error, Please Try Again" });
+            fs.unlinkSync(req.file.path);
             console.log(error);
         }
         fs.unlinkSync(req.file.path);
